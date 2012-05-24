e32daaa7521827a55e397152613480bd5f418fd5
Hasan Iskandar <hasan.iskandar@gmail.com>
2012-05-25 00:30:02 +0700

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ViralvelocityTestapp::Application.routes.draw do
+  root :to => 'home#index'
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
