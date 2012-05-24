51239ee29729ebff787a99764dccb202f340bee1
Hasan Iskandar <hasan.iskandar@gmail.com>
2012-05-25 01:12:42 +0700

    install twitter-bootstrap-rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_messages
+    flash.map do |type, message|
+      alert_class = case type
+      when :notice then :info
+      when :alert, :warning then :error
+      else type
+      end
+      content_tag :div, :class => "alert alert-#{alert_class} fade in" do
+        content_tag(:button, raw("&times;").html_safe, :class => "close", :"data-dismiss" => "alert") + h(message)
+      end
+    end.join("\n").html_safe
+  end
 end
