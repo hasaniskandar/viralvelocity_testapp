f225eca28dc92f19cfef17c1dc8c0ccaa0f2e850
Hasan Iskandar <hasan.iskandar@gmail.com>
2012-05-25 00:11:25 +0700

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViralvelocityTestapp::Application.config.secret_token = '6d6d6650153434821e6b401412fab2ada2a80210d6e3a737a7721d2f70b78e2ad34c5365d361b4613c54103756765d9778762628f0e2f6579749db62e5dc8d50'
